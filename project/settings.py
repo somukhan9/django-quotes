9c4484ac343de80eeee7b6b9947823944d78c86e
somukhan <mhsomrat154@gmail.com>
2023-03-04 22:32:02 +0600

    change allowed host

--- project/settings.py
+++ project/settings.py
@@ -30,9 +30,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6kj&bde=shm+pz$x8=uq+5#4#-(2-4du4_c@n!hda6vszn4x0#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.onrender.com']
 
 
 # Application definition
